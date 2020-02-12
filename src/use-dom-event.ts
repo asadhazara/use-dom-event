cb0a81ec392116a651da71e151cfb444280a8811
Asad Hazara <asadhazara@hotmail.com>
2020-02-12 13:17:15 +0100

    fix: 🐛 reattempt to fix issue saying target is null

--- src/use-dom-event.ts
+++ src/use-dom-event.ts
@@ -99,12 +99,12 @@ export function useDOMEvent<K extends string>(
   deps?: React.DependencyList,
 ): void {
   useEffect(() => {
-    if (target) {
-      target.addEventListener(type, listener, options);
+    if (!target) return;
 
-      return (): void => {
-        target.removeEventListener(type, listener, options);
-      };
-    }
+    target.addEventListener(type, listener, options);
+
+    return (): void => {
+      target.removeEventListener(type, listener, options);
+    };
   }, deps || []); // eslint-disable-line react-hooks/exhaustive-deps
 }
